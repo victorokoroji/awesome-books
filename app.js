cea4d206ab0899c2b4d5e8c646b1601acf02b7cb
unknown <okorojiebube2@gmail.com>
2022-02-17 06:27:28 -0600

    Modify app.js

--- app.js
+++ app.js
@@ -1,65 +1,69 @@
-const form = document.querySelector('#form');
+const form = document.querySelector('#form')
 
-function removeBook(val) {
-  const removeItem = val.split('-');
-  let [id] = removeItem[1];
-  id = parseInt(id, 10);
-  let bookList = JSON.parse(localStorage.getItem('book-data'));
-  bookList = bookList.filter((word, index) => index !== id);
-  localStorage.setItem('book-data', JSON.stringify(bookList));
-  const targetElement = document.getElementById(`item-${id}`);
-  targetElement.remove();
-}
+class Book {
+	constructor(title, author) {
+		this.title = title
+		this.author = author
+	}
 
-function fillBooks() {
-  const getBooks = JSON.parse(localStorage.getItem('book-data'));
-  if (getBooks !== null && getBooks.length > 0) {
-    document.querySelector('.book-list').innerHTML = getBooks
-      .map(
-        (book, index) => `
-<div class="book-item" id="item-${index}">
-<label for="name">${book.title}</label>
-<br>
-<label for="author">${book.author}</label>
-<br>
-<button type="button" id="btn-${index}" class="remove-btn">Remove</button>
-<hr>
-</div>
+	removeBook = val => {
+		const removeItem = val.split('-')
+		let [id] = removeItem[1]
+		id = parseInt(id, 10)
+		let bookList = JSON.parse(localStorage.getItem('book-data'))
+		bookList = bookList.filter((word, index) => index !== id)
+		localStorage.setItem('book-data', JSON.stringify(bookList))
+		const targetElement = document.getElementById(`item-${id}`)
+		targetElement.remove()
+	}
+
+	fillBooks() {
+		const strBookList = JSON.parse(localStorage.getItem('book-data'))
+		if (strBookList !== null && strBookList.length > 0) {
+			document.querySelector('.book-list').innerHTML = strBookList
+				.map(
+					(book, index) => `
+<tr class="book-item" id="item-${index}">
+<td>"${book.title}" by ${book.author} </td>
+<td><button type="button" id="btn-${index}" class="remove-btn">Remove</button> </td>
+</tr>
 `,
-      )
-      .join('');
+				)
+				.join('')
 
-    document.querySelectorAll('.remove-btn').forEach((val) => {
-      val.addEventListener('click', (event) => {
-        const val = event.target.id;
-        removeBook(val);
-      });
-    });
-  } else {
-    document.querySelector('.book-list').innerHTML = '';
-  }
+			document.querySelectorAll('.remove-btn').forEach(val => {
+				val.addEventListener('click', event => {
+					const val = event.target.id
+					this.removeBook(val)
+				})
+			})
+		} else {
+			document.querySelector('.book-list').innerHTML = ''
+		}
+	}
+
+	storeBooks = e => {
+		e.preventDefault()
+		let existingBooks = JSON.parse(localStorage.getItem('book-data'))
+		const title = form.elements.name.value
+		const author = form.elements.author.value
+		const book = { title, author }
+
+		existingBooks = existingBooks === null ? [] : existingBooks
+
+		if (
+			!(existingBooks.filter(book => book.title === title && book.author === author).length > 0)
+		) {
+			existingBooks.push(book)
+		}
+		localStorage.setItem('book-data', JSON.stringify(existingBooks))
+		form.elements.name.value = ''
+		form.elements.author.value = ''
+		this.fillBooks()
+	}
 }
 
-fillBooks();
+const bookObj = new Book()
+bookObj.fillBooks()
 
-function storeBooks(e) {
-  e.preventDefault();
-  let existingBooks = JSON.parse(localStorage.getItem('book-data'));
-  const title = form.elements.name.value;
-  const author = form.elements.author.value;
-  const book = { title, author };
-
-  existingBooks = existingBooks === null ? [] : existingBooks;
-
-  if (
-    !(existingBooks.filter((book) => book.title === title && book.author === author).length > 0)
-  ) {
-    existingBooks.push(book);
-  }
-  localStorage.setItem('book-data', JSON.stringify(existingBooks));
-  form.elements.name.value = '';
-  form.elements.author.value = '';
-  fillBooks();
-}
-
-form.addEventListener('submit', storeBooks);
+form.addEventListener('submit', bookObj.storeBooks)
