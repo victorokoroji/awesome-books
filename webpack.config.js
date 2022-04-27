b158fa3ce33ebb2521a489b944723b9dddf85b8e
unknown <okorojiebube2@gmail.com>
2022-04-27 16:44:50 -0600

    Build app

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,42 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  devServer: {
-    static: './dist',
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/inline',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/inline',
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      favicon: './src/images/favicon.ico',
-      filename: 'index.html',
-    }),
-  ],
-};
+	mode: 'production',
+	entry: './src/index.js',
+	devServer: {
+		static: './dist',
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/inline',
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/inline',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			favicon: './src/images/favicon.ico',
+			filename: 'index.html',
+		}),
+		new CopyPlugin({
+			patterns: [{ from: './src/images/screenshot.png', to: 'images' }],
+		}),
+	],
+}
